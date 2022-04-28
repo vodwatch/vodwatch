7ebad196228b18503d5726de6a20a5f6033706db
Piotr Tomczyk <93157684+piotr-tomczyk@users.noreply.github.com>
2022-04-28 13:05:30 +0200

    Fix reload issue

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,8 @@ console.log("Hi I'm background script!");
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   let substring = "https://www.netflix.com/watch";
   if (changeInfo.url?.includes(substring)) {
-    chrome.tabs.reload(tabId);
-    console.log("WATCHING VIDEO");
+    chrome.tabs.sendMessage(tabId, { text: "on video" }, (message) => {
+      return;
+    });
   }
 });

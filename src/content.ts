f2099eae19cb9204b5b88a6004b694a39ef7f35b
Michal Januszkiewicz <michaljanuszkiewicz6@gmail.com>
2022-04-29 12:36:08 +0200

    Add event handlers for video controls, closes #19, closes #20, closes #21

--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,24 @@
+import { waitForElementToLoad } from './utils/utils';
+
+const handleVideoEvent = (event: Event) => {
+  const video = event.target as HTMLVideoElement
+  console.log(video.currentTime);
+  console.log(event.type);
+}
+
+const waitForVideoToLoad = async () => {
+  const video = await waitForElementToLoad('video');
+  if (video) {
+    video.addEventListener("play", handleVideoEvent);
+    video.addEventListener("pause", handleVideoEvent);
+    video.addEventListener("seeked", handleVideoEvent);
+  }
+}
+
 console.log("Hi, I'm content script!");
 let substring = "https://www.netflix.com/watch";
 if (document.location.href.includes(substring)) {
-  let video = document.getElementsByTagName("video");
-  console.log(video, "from content!");
+  waitForVideoToLoad();
 }
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   let video = document.getElementsByTagName("video");

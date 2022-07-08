bd525a36f6c1f0c44b6564eda835d9dff0281718
Piotr Tomczyk <93157684+piotr-tomczyk@users.noreply.github.com>
2022-07-08 07:19:39 +0200

    Add receiving permissions from the server and TRY to execute them

--- src/modules/socket.ts
+++ src/modules/socket.ts
@@ -1,123 +1,148 @@
 import { io, Socket } from "socket.io-client";
-import { EventInfo } from "./video"
+import { EventInfo } from "./video";
 
 const SocketEventType = {
-    MESSAGE: "message",
-    SEND_VIDEO_EVENT: "send_video_event",
-    RECEIVE_VIDEO_EVENT: "receive_video_event",
-    CREATE_ROOM: "create_room",
-    JOIN_ROOM: "join_room",
-    FIND_ROOM_BY_CLIENT: "find_room_by_client",
+  MESSAGE: "message",
+  SEND_VIDEO_EVENT: "send_video_event",
+  RECEIVE_VIDEO_EVENT: "receive_video_event",
+  CREATE_ROOM: "create_room",
+  JOIN_ROOM: "join_room",
+  FIND_ROOM_BY_CLIENT: "find_room_by_client",
+  PERMISSIONS: "permissions",
 };
+interface Permissions {
+  pause: boolean;
+  play: boolean;
+  seek: boolean;
+  chat: boolean;
+  kick: boolean;
+}
 
 export class ClientSocketHandler {
+  private serverUrl: string;
+  private socket!: Socket;
+  private video: HTMLVideoElement;
+  private permissions!: Permissions;
 
-    private serverUrl: string;
-    private socket!: Socket;
-    private video: HTMLVideoElement;
+  constructor(video: HTMLVideoElement) {
+    this.serverUrl = "http://localhost:5000";
+    this.video = video;
+  }
 
-    constructor(video: HTMLVideoElement) {
-        this.serverUrl = "http://localhost:5000";
-        this.video = video;
-    }
-
-    openConnection = () => {
-        this.socket = io(this.serverUrl);
-        this.socket.on("connect", () => {
-            console.log("Socket is connected");
-        });
-        this.socket.on("disconnect", () => {
-            console.log("Socket is disconnected");
-        });
-        this.socket.on(SocketEventType.RECEIVE_VIDEO_EVENT, (message: EventInfo) => {
-            console.log("Received video event from the server: ", message);
-            switch (message.event) {
-                case "play":
-                    this.video.play();
-                    console.log("Video is played!");
-                    break;
-                case "pause":
-                    this.video.pause();
-                    console.log("Video is paused!");
-                    break;
-                case "seeked":
-                    //this.video.currentTime = message.currentTime;
-                    console.log("Video is seeked!", message.currentTime);
-                    break;
-            }
-        });
-    }
-
-    sendMessage = (message: string) => {
-        this.checkForErrors();
-        this.socket.emit(SocketEventType.MESSAGE, message, (response: string) => {
-            console.log("Response: " + response);
-        })
-    }
-
-    sendVideoEvent = (eventInfo: EventInfo) => {
-        this.checkForErrors();
-        const myRoomId = this.findMyRoom();
-        if (myRoomId == null)
-            throw new Error("You are not in a room!");
-        const data = {
-            eventInfo,
-            myRoomId
+  openConnection = () => {
+    this.socket = io(this.serverUrl);
+    this.socket.on("connect", () => {
+      console.log("Socket is connected");
+    });
+    this.socket.on("disconnect", () => {
+      console.log("Socket is disconnected");
+    });
+    this.socket.on(
+      SocketEventType.RECEIVE_VIDEO_EVENT,
+      (message: EventInfo) => {
+        console.log("Received video event from the server: ", message);
+        switch (message.event) {
+          case "play":
+            this.video.play();
+            console.log("Video is played!");
+            break;
+          case "pause":
+            this.video.pause();
+            console.log("Video is paused!");
+            break;
+          case "seeked":
+            //this.video.currentTime = message.currentTime;
+            console.log("Video is seeked!", message.currentTime);
+            break;
         }
-        console.log(data);
-        this.socket.emit(SocketEventType.SEND_VIDEO_EVENT, data, (response: string) => {
-            console.log("Response: " + response);
-        })
-    }
+      }
+    );
+    this.socket.on(SocketEventType.PERMISSIONS, (message: any) => {
+      console.log("Received video event from the server: ", message);
+      this.permissions = message.permissions;
+    });
+  };
 
-    joinRoom = (roomId: string) => {
-        this.checkForErrors();
-        return new Promise((resolve, reject) => {
-            this.socket.emit(SocketEventType.JOIN_ROOM, roomId, (response: string) => {
-                console.log("Response: " + response);
-                if (response === "ROOM_NOT_FOUND") {
-                    reject(response);
-                }
-                resolve(response);
-            })
-        });
-    }
+  sendMessage = (message: string) => {
+    this.checkForErrors();
+    this.socket.emit(SocketEventType.MESSAGE, message, (response: string) => {
+      console.log("Response: " + response);
+    });
+  };
 
-    createRoom = (roomId: string) => {
-        this.checkForErrors();
-        return new Promise((resolve, reject) => {
-            this.socket.emit(SocketEventType.CREATE_ROOM, roomId, (response: string) => {
-                console.log("Response: " + response);
-                if (response === "ROOM_ALREADY_EXISTS") {
-                    reject(response);
-                }
-                resolve(response);
-            })
-        })
-    }
+  sendVideoEvent = (eventInfo: EventInfo) => {
+    this.checkForErrors();
+    const myRoomId = this.findMyRoom();
+    if (myRoomId == null) throw new Error("You are not in a room!");
+    const data = {
+      eventInfo,
+      myRoomId,
+    };
+    console.log(data);
+    this.socket.emit(
+      SocketEventType.SEND_VIDEO_EVENT,
+      data,
+      (response: string) => {
+        console.log("Response: " + response);
+      }
+    );
+  };
 
-    findMyRoom = () => {
-        this.checkForErrors();
-        return new Promise((resolve, reject) => {
-            this.socket.emit(SocketEventType.FIND_ROOM_BY_CLIENT, (myRoomId: string) => {
-                console.log("Response: " + myRoomId);
-                if (myRoomId === "ROOM_NOT_FOUND") {
-                    reject(myRoomId);
-                }
-                resolve(myRoomId);
-            })
-        });
-    }
+  joinRoom = (roomId: string) => {
+    this.checkForErrors();
+    return new Promise((resolve, reject) => {
+      this.socket.emit(
+        SocketEventType.JOIN_ROOM,
+        roomId,
+        (response: string) => {
+          console.log("Response: " + response);
+          if (response === "ROOM_NOT_FOUND") {
+            reject(response);
+          }
+          resolve(response);
+        }
+      );
+    });
+  };
 
-    closeConnection = () => {
-        this.checkForErrors();
-        this.socket.close();
-    }
+  createRoom = (roomId: string) => {
+    this.checkForErrors();
+    return new Promise((resolve, reject) => {
+      this.socket.emit(SocketEventType.CREATE_ROOM, roomId, (response: any) => {
+        console.log("Response: " + response);
+        if (response === "ROOM_ALREADY_EXISTS") {
+          reject(response);
+        }
+        resolve(response);
+      });
+    });
+  };
 
-    private checkForErrors = () => {
-        if (!this.socket)
-            throw new Error("Socket is not initialized");
-        if (!this.socket.connected)
-            throw new Error("Socket is not connected");
-    }
+  findMyRoom = () => {
+    this.checkForErrors();
+    return new Promise((resolve, reject) => {
+      this.socket.emit(
+        SocketEventType.FIND_ROOM_BY_CLIENT,
+        (myRoomId: string) => {
+          console.log("Response: " + myRoomId);
+          if (myRoomId === "ROOM_NOT_FOUND") {
+            reject(myRoomId);
+          }
+          resolve(myRoomId);
+        }
+      );
+    });
+  };
+
+  closeConnection = () => {
+    this.checkForErrors();
+    this.socket.close();
+  };
+
+  getPermissions = (): Permissions => this.permissions;
+
+  private checkForErrors = () => {
+    if (!this.socket) throw new Error("Socket is not initialized");
+    if (!this.socket.connected) throw new Error("Socket is not connected");
+  };
 }
